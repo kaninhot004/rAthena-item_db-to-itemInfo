d32d06e3dd04f645d1b16d7f13fbea0b77db7c59
Kanin Temsrisuk <kaifa2910@msn.com>
2025-07-06 14:14:22 +0700

    Debug text.

--- Assets/Scripts/SpriteRobeFixer.cs
+++ Assets/Scripts/SpriteRobeFixer.cs
@@ -13,6 +13,7 @@ public class SpriteRobeFixer : MonoBehaviour
     public const string FEMALE_ENCODE_874 = "ฟฉ";
 
     [SerializeField] Button _fix;
+    [SerializeField] Text _status;
 
     private void Start()
     {
@@ -22,49 +23,59 @@ public class SpriteRobeFixer : MonoBehaviour
     [Button()]
     public void Fix()
     {
+        int fixCount = 0;
+
         List<string> maleList = new List<string>();
         List<string> femaleList = new List<string>();
 
+        _status.text = "Fetching directory 'male' job name";
         DirectoryInfo dir = new DirectoryInfo(JOB_NAME_PATH + MALE_ENCODE_874);
+        _status.text = "Fetching files 'male' job name";
         var files = dir.GetFiles("*");
+        _status.text = "Adding 'male' job name";
         foreach (var file in files)
         {
             if (!maleList.Contains(file.Name) && !file.Name.Contains(".meta"))
                 maleList.Add(file.Name);
         }
 
+        _status.text = "Fetching directory 'female' job name";
         dir = new DirectoryInfo(JOB_NAME_PATH + FEMALE_ENCODE_874);
+        _status.text = "Fetching files 'female' job name";
         files = dir.GetFiles("*");
+        _status.text = "Adding 'female' job name";
         foreach (var file in files)
         {
             if (!femaleList.Contains(file.Name) && !file.Name.Contains(".meta"))
                 femaleList.Add(file.Name);
         }
 
+        _status.text = "Fetching 'to fix' folders";
         dir = new DirectoryInfo(TO_FIX_PATH);
         var folders = dir.GetDirectories("*");
         foreach (var folder in folders)
         {
             string sprToCopy = string.Empty;
             string actToCopy = string.Empty;
-
+            string lastJobFileName = string.Empty;
+            _status.text = "Fetching 'to fix' folders " + folder.Name;
             foreach (var jobFileName in maleList)
             {
+                lastJobFileName = jobFileName;
                 var checkPath = TO_FIX_PATH + folder.Name + "/" + MALE_ENCODE_874 + "/" + jobFileName;
-
+                _status.text = "Fetching 'to fix' folders " + folder.Name + " spr file to copy";
                 if (File.Exists(checkPath) && checkPath.Contains(".spr"))
                     sprToCopy = checkPath;
-
+                _status.text = "Fetching 'to fix' folders " + folder.Name + " act file to copy";
                 if (File.Exists(checkPath) && checkPath.Contains(".act"))
                     actToCopy = checkPath;
-
                 if (!string.IsNullOrEmpty(sprToCopy) && !string.IsNullOrEmpty(actToCopy))
                     break;
             }
 
             if (string.IsNullOrEmpty(sprToCopy) || string.IsNullOrEmpty(actToCopy))
             {
-                Debug.LogWarning("Can't fix male folder " + folder.Name + " because not found " + (string.IsNullOrEmpty(sprToCopy) ? "spr" : "act") + " to copy");
+                _status.text = "Can't fix 'male' folder " + folder.Name + " because not found " + (string.IsNullOrEmpty(sprToCopy) ? "spr" : "act") + " to copy\nPath:" + TO_FIX_PATH + folder.Name + "/" + MALE_ENCODE_874 + "/" + lastJobFileName;
                 return;
             }
             else
@@ -75,15 +86,15 @@ public class SpriteRobeFixer : MonoBehaviour
 
                     if (!File.Exists(checkPath) && checkPath.Contains(".spr"))
                     {
-                        Debug.Log("Fixing spr male folder " + folder.Name + " file name " + jobFileName);
-
+                        fixCount++;
+                        _status.text = "Fixing spr 'male' folder " + folder.Name + " file name " + jobFileName;
                         File.Copy(sprToCopy, TO_FIX_PATH + folder.Name + "/" + MALE_ENCODE_874 + "/" + jobFileName);
                     }
 
                     if (!File.Exists(checkPath) && checkPath.Contains(".act"))
                     {
-                        Debug.Log("Fixing act male folder " + folder.Name + " file name " + jobFileName);
-
+                        fixCount++;
+                        _status.text = "Fixing act 'male' folder " + folder.Name + " file name " + jobFileName;
                         File.Copy(actToCopy, TO_FIX_PATH + folder.Name + "/" + MALE_ENCODE_874 + "/" + jobFileName);
                     }
                 }
@@ -92,23 +103,23 @@ public class SpriteRobeFixer : MonoBehaviour
             sprToCopy = string.Empty;
             actToCopy = string.Empty;
 
+            _status.text = "Fetching 'to fix' folders " + folder.Name;
             foreach (var jobFileName in femaleList)
             {
                 var checkPath = TO_FIX_PATH + folder.Name + "/" + FEMALE_ENCODE_874 + "/" + jobFileName;
-
+                _status.text = "Fetching 'to fix' folders " + folder.Name + " spr file to copy";
                 if (File.Exists(checkPath) && checkPath.Contains(".spr"))
                     sprToCopy = checkPath;
-
+                _status.text = "Fetching 'to fix' folders " + folder.Name + " act file to copy";
                 if (File.Exists(checkPath) && checkPath.Contains(".act"))
                     actToCopy = checkPath;
-
                 if (!string.IsNullOrEmpty(sprToCopy) && !string.IsNullOrEmpty(actToCopy))
                     break;
             }
 
             if (string.IsNullOrEmpty(sprToCopy) || string.IsNullOrEmpty(actToCopy))
             {
-                Debug.LogWarning("Can't fix female folder " + folder.Name + " because not found " + (string.IsNullOrEmpty(sprToCopy) ? "spr" : "act") + " to copy");
+                _status.text = "Can't fix 'female' folder " + folder.Name + " because not found " + (string.IsNullOrEmpty(sprToCopy) ? "spr" : "act") + " to copy";
                 return;
             }
             else
@@ -119,19 +130,21 @@ public class SpriteRobeFixer : MonoBehaviour
 
                     if (!File.Exists(checkPath) && checkPath.Contains(".spr"))
                     {
-                        Debug.Log("Fixing spr female folder " + folder.Name + " file name " + jobFileName);
-
+                        fixCount++;
+                        _status.text = "Fixing spr 'female' folder " + folder.Name + " file name " + jobFileName;
                         File.Copy(sprToCopy, TO_FIX_PATH + folder.Name + "/" + FEMALE_ENCODE_874 + "/" + jobFileName);
                     }
 
                     if (!File.Exists(checkPath) && checkPath.Contains(".act"))
                     {
-                        Debug.Log("Fixing act female folder " + folder.Name + " file name " + jobFileName);
-
+                        fixCount++;
+                        _status.text = "Fixing act 'female' folder " + folder.Name + " file name " + jobFileName;
                         File.Copy(actToCopy, TO_FIX_PATH + folder.Name + "/" + FEMALE_ENCODE_874 + "/" + jobFileName);
                     }
                 }
             }
         }
+
+        _status.text = "Done fix " + fixCount + " sprite robe";
     }
 }
